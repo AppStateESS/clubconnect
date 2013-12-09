3073e4d31312b0a43789c7770a567039b6d01bc9
Jeff Tickle <ticklejw@appstate.edu>
2013-12-09 08:57:35 -0500

    Fixing an alignment issue with long memberships

--- class/TranscriptPDFGenerator.php
+++ class/TranscriptPDFGenerator.php
@@ -19,10 +19,10 @@ class TranscriptPDFGenerator extends TranscriptView
     function __construct($transcript)
     {
         parent::__construct($transcript);
-    
+
         $this->settings = new TranscriptPrintSettings(UserStatus::getUsername());
         $this->membershipsForTerm = array();
-	$this->col = 0;
+        $this->col = 0;
     }
 
     function show()
@@ -56,65 +56,46 @@ class TranscriptPDFGenerator extends TranscriptView
         // Should an entire term ever take up more than one page... ohes noes.
         // Also, there is probably some sort of corner case here.  Just be careful.
         $curpos = $this->pdf->GetY();
-	$y0 = $curpos;
+        $y0 = $curpos;
         $newpos = $curpos + (5 * (count($this->membershipsForTerm) + 1));
         if($newpos > $this->pdf->PageBreakTrigger) {
-	    //this is where we need to do the column stuffs
-	    //instead of adding a page here we need to go to the next column
-	    // Method accepting or not automatic page break
-	    if($this->col<1) {
+            //this is where we need to do the column stuffs
+            //instead of adding a page here we need to go to the next column
+            // Method accepting or not automatic page break
+            if($this->col<1) {
                 // Go to next column
                 $this->SetCol($this->col+1);
                 // Set y to top
                 $this->pdf->SetY(56); //magic number!
                 // Keep on page
-	    }
-	    else {
-		// Go back to first column
-		$this->SetCol(0);
-		// Page break
-		$this->pdf->AddPage();
-	    }
-	}
+            }
+            else {
+                // Go back to first column
+                $this->SetCol(0);
+                // Page break
+                $this->pdf->AddPage();
+            }
+        }
 
         $this->pdf->setFont('Arial', 'B', 14);
-        $this->pdf->MultiCell(90, 5, Term::toString($term) . "\n");
+        $this->pdf->MultiCell(80, 5, Term::toString($term) . "\n", 0, 'L');
         $this->pdf->setFont('Arial', NULL, 10);
 
         foreach($this->membershipsForTerm as $m) {
-	    $this->pdf->MultiCell(90, 5, $m); //change 60 back to 0
+            $this->pdf->MultiCell(90, 5, $m); //change 60 back to 0
         }
-	$this->pdf->Ln();
+        $this->pdf->Ln();
 
         $this->membershipsForTerm = array();
     }
-    /*protected function acceptPageBreak()
-    {
-	// Method accepting or not automatic page break
-	if($this->col<1)
-	{
-	    // Go to next column
-	    $this->SetCol($this->col+1);
-	    // Set ordinate to top
-	    $this->SetY($this->y0);
-	    // Keep on page
-	    return false;
-	}
-	else
-	{
-	    // Go back to first column
-	    $this->SetCol(0);
-	    // Page break
-	    return true;
-	}
-	}*/
+
     protected function SetCol($col)
     {
-	// Set position at a given column
-	$this->col = $col;
-	$x = 10+$col*95;
-	$this->pdf->SetLeftMargin($x);
-	$this->pdf->SetX($x);
+        // Set position at a given column
+        $this->col = $col;
+        $x = 10+$col*95;
+        $this->pdf->SetLeftMargin($x);
+        $this->pdf->SetX($x);
     }
 }
 
